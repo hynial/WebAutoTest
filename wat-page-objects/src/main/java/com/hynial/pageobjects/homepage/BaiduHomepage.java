983debcb160dc7e04aec79197a9ff3d10ea68eaf
hynial.chen <hynials@gmail.com>
2021-02-07 14:37:20 +0800

    add logback support

--- wat-page-objects/src/main/java/com/hynial/pageobjects/homepage/BaiduHomepage.java
+++ wat-page-objects/src/main/java/com/hynial/pageobjects/homepage/BaiduHomepage.java
@@ -2,9 +2,14 @@ package com.hynial.pageobjects.homepage;
 
 import com.hynial.framework.core.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BaiduHomepage extends BasePage {
+    private Logger logger = LoggerFactory.getLogger(BaiduHomepage.class);
+
     public BaiduHomepage(WebDriver driver) {
         super(driver);
+        logger.info("this is baidu  home page {}", Thread.currentThread().getId());
     }
 }
--- wat-regression-tests/src/test/java/com/hynial/cucumbertest/TestNGRunner.java
+++ wat-regression-tests/src/test/java/com/hynial/cucumbertest/TestNGRunner.java
@@ -2,6 +2,8 @@ package com.hynial.cucumbertest;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @CucumberOptions(
         dryRun = false,
@@ -12,5 +14,8 @@ import io.cucumber.testng.CucumberOptions;
         publish = false
 )
 public class TestNGRunner extends AbstractTestNGCucumberTests {
-
+    private static final Logger logger = LoggerFactory.getLogger(TestNGRunner.class);
+    public TestNGRunner(){
+        logger.info("new a testng runner");
+    }
 }
